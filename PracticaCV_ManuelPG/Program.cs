6a07769c14a5a19889b852eb0e320b373262876c
Alumno <Alumno@DAM1-PC9>
2026-02-18 08:34:51 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV_ManuelPG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_ManuelPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
